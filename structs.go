270989834a41a2180d9e92385a254b44edfcac14
Bombwhale <solkblte@icloud.com>
2018-11-18 21:04:45 +0900

    godoc support

--- structs.go
+++ structs.go
@@ -7,12 +7,14 @@ type School struct {
 	Zone           string // 학교를 관할하는 교육청
 }
 
+// GetWeekMeal 함수의 두번째 인자로 들어가는 mealtype에 사용하는 상수들입니다.
 const (
 	Breakfast = iota + 1 // 아침
 	Lunch                // 점심
 	Dinner               // 저녁
 )
 
+// School 구조체의 SchoolKindCode에 사용할 수 있습니다.
 const (
 	Kindergarden = iota + 1 // 유치원
 	Elementary              // 초등학교
@@ -20,7 +22,7 @@ const (
 	High                    // 고등학교
 )
 
-// 교육청의 도메인
+// School 구조체의 Zone 필드에 사용 가능합니다.
 const (
 	Seoul     = "sen"
 	Busan     = "pen"
