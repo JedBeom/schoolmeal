6fe1528ce9a76ed810ec0bb5e95715ad22763e46
Bombwhale <solkblte@icloud.com>
2018-12-07 21:19:13 +0900

    Timestamp changed

--- utils.go
+++ utils.go
@@ -6,11 +6,11 @@ import (
 )
 
 // Timestamp 함수는 GetWeekMeal 함수의 첫번째 인자로 쓰기 좋습니다.
-// 현재의 시간을 2018.11.18의 형태로 리턴합니다.
-func Timestamp() (date string) {
+// 인자로 받은 시간을 2018.11.18의 형태로 리턴합니다.
+func Timestamp(date time.Time) (stamp string) {
 	format := "%d.%d.%d"
-	y, m, d := time.Now().Date()
+	y, m, d := date.Date()
 
-	date = fmt.Sprintf(format, y, m, d)
+	stamp = fmt.Sprintf(format, y, m, d)
 	return
 }
